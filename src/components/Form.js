76f2debbde380ce11c79a7fd5ead453734d86e01
Swikriti Tripathi <41103328+SwikritiT@users.noreply.github.com>
2022-06-25 18:15:23 +0545

    Update README.md

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 
-const Form = ({setInputText, todos, setTodos, inputText,setStatus}) =>{
+const Form = ({setInputText,setInputTag, todos, setTodos, inputText,setStatus,inputTag}) =>{
     const inputTextHandler = (e) =>{
         setInputText(e.target.value);
+    }
+    const inputTagHandler = (e) =>{
+        setInputTag(e.target.value);
     }
     const submitToDoHandler = (e) => {
         e.preventDefault();
         setTodos([
             ...todos,
-            {text: inputText, completed:false, id: Math.random()*1000}
+            {text: inputText, completed:false, id: Math.random()*1000,tag:inputTag}
         ]);
         setInputText("");
+        setInputTag("");
     }
     const statusHandler = (e) =>{
         setStatus(e.target.value);
@@ -20,6 +24,7 @@ const Form = ({setInputText, todos, setTodos, inputText,setStatus}) =>{
     return(
         <form>
             <input onChange= {inputTextHandler} type="text" className="todo-input" value={inputText}/>
+            <input onChange= {inputTagHandler} type="text" className="todo-tag" value={inputTag}/>
             <button onClick = {submitToDoHandler} disabled={!inputText} className="todo-button" type="submit">
                 <i className="fas fa-plus-square" ></i>
             </button>
